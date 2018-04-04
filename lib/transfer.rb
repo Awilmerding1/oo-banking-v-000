bb799a9afd594e60570d02e9f731514aa7aee95a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-04 14:17:24 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
   
   def execute_transaction
-    @sender.valid? == false 
+    if @sender.valid? == false 
      "Transaction rejected. Please check your account balance."
    end
     until self.status == "complete"
