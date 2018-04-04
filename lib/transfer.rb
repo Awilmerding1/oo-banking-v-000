33d07e0feab734c025d078d8a9cd524c258ade7b
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-04 14:38:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,6 +28,7 @@ class Transfer
     self.status = "complete"
     end
     when false
+      binding.pry
     self.status = "rejected"
     "Transaction rejected. Please check your account balance."
   end
